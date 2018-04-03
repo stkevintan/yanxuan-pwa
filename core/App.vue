c0a311609b5cc5ad9d57d9963c590ad855b01f25
stkevintan <stkevintan@zju.edu.cn>
2018-04-03 16:53:08 +0800

    initial

---
+++ core/App.vue
@@ -0,0 +1,115 @@
+<template>
+    <div id="app">
+        <transition
+            :name="pageTransitionEffect">
+            <keep-alive
+                :include="[...keepAlivePages]">
+                <router-view
+                    :key="routerViewKey"
+                    class="app-view"
+                    :class="[pageTransitionClass]"
+                    ></router-view>
+            </keep-alive>
+        </transition>
+        <update-toast></update-toast>
+    </div>
+</template>
+
+<script>
+import {mapState} from 'vuex';
+import UpdateToast from '@/components/UpdateToast';
+import {keepAlivePages} from '@/.lavas/router';
+
+export default {
+    name: 'app',
+    components: {
+        UpdateToast
+    },
+    computed: {
+        ...mapState('pageTransition', {
+            pageTransitionType: state => state.type,
+            pageTransitionEffect: state => state.effect
+        }),
+
+        pageTransitionClass() {
+            return `transition-${this.pageTransitionType}`;
+        },
+
+        // https://github.com/lavas-project/lavas/issues/119
+        routerViewKey() {
+            let {name, params} = this.$route;
+            let paramKeys = Object.keys(params);
+            if (paramKeys.length) {
+                return name + paramKeys.reduce((prev, cur) => prev + params[cur], '');
+            }
+            return null;
+        }
+    },
+    data() {
+        return {
+            // https://github.com/lavas-project/lavas/issues/112
+            keepAlivePages
+        };
+    }
+};
+</script>
+
+<style lang="stylus">
+#app
+    font-family 'Avenir', Helvetica, Arial, sans-serif
+    -webkit-font-smoothing antialiased
+    -moz-osx-font-smoothing grayscale
+    text-align center
+    color #2c3e50
+
+    .app-view
+        position absolute
+        top 0
+        right 0
+        bottom 0
+        left 0
+        overflow-x hidden
+        overflow-y auto
+        background white
+
+        &::-webkit-scrollbar
+            width 0
+            background transparent
+
+        &.transition-slide
+            transition transform 0.4s cubic-bezier(.55, 0, .1, 1)
+
+            &.slide-left-enter
+                transform translate(100%, 0)
+
+            &.slide-right-enter
+                transform translate(-100%, 0)
+
+            &.slide-right-leave-active
+                transform translate(100%, 0)
+
+            &.slide-left-leave-active
+                transform translate(-100%, 0)
+
+        &.transition-fade
+            opacity 1
+            transition opacity 1s ease
+
+            &.fade-enter
+                opacity 0
+
+            &.fade-leave-active
+                opacity 0
+
+        // &.transition-slide-fade
+        //     &.slide-fade-enter-active
+        //         transition: all .3s ease
+        //
+        //     &.slide-fade-leave-active
+        //         transition: all .8s cubic-bezier(1.0, 0.5, 0.8, 1.0)
+        //
+        //     &.slide-fade-enter,
+        //     &.slide-fade-leave-to
+        //         transform: translateX(10px)
+        //         opacity: 0
+</style>
---
+++ lavas.config.js
@@ -0,0 +1,50 @@
+/**
+ * @file lavas config
+ * @author stkevintan(stkevintan@zju.edu.cn)
+ */
+
+'use strict';
+
+const path = require('path');
+const BUILD_PATH = path.resolve(__dirname, 'dist');
+const isDev = process.env.NODE_ENV === 'development';
+const isProd = process.env.NODE_ENV === 'production';
+
+module.exports = {
+    build: {
+        ssr: false,
+        path: BUILD_PATH,
+        publicPath: '/',
+        ssrCopy: isDev ? [] : [
+            {
+                src: 'server.prod.js'
+            },
+            {
+                src: 'package.json'
+            }
+        ]
+    },
+    router: {
+        mode: 'history',
+        base: '/',
+        pageTransition: {
+            type: 'fade',
+            transitionClass: 'fade'
+        }
+    },
+    serviceWorker: {
+        swSrc: path.join(__dirname, 'core/service-worker.js'),
+        swDest: path.join(BUILD_PATH, 'service-worker.js'),
+        // swPath: '/custom_path/', // specify custom serveice worker file's path, default is publicPath
+        globDirectory: BUILD_PATH,
+        globPatterns: [
+            '**/*.{html,js,css,eot,svg,ttf,woff}'
+        ],
+        globIgnores: [
+            'sw-register.js',
+            '**/*.map'
+        ],
+        appshellUrl: '/appshell',
+        dontCacheBustUrlsMatching: /\.\w{8}\./
+    }
+};
